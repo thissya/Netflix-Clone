4ad59b657f5cfa36b51b2cf397dbd878bc34d7de
thissya <thissya129@gmail.com>
2025-04-22 01:12:02 +0530

    signup page

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
+import{ Link }from 'react-router-dom';
 
 function Navbar(){
   return (
     <div className="flex items-center justify-between p-4 z-[100] w-full absolute md:p-8 ">
+      <Link to ='/'>
         <h1 className="text-red-600 font-bold cursor-pointer text-4xl">NETFLIX</h1>
+      </Link>
         <div >
+          <Link to='/signup'>
             <button className="text-white px-6 py-2 rounded cursor-pointer">Sign In</button>
+          </Link>
+          <Link to='/login'>
             <button className="text-white bg-red-600 px-6 py-2 rounded cursor-pointer">Sign Up</button>
+          </Link>
         </div>
     </div>
   )
--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,7 +1,13 @@
 import React from 'react'
 
 export default function Signup() {
-  return (
-    <div>Signup</div>
-  )
+    return (
+        <>
+            <div className="w-full h-screen">
+                <img className="sm:block h-full w-full absolute object-cover" src="https://assets.nflxext.com/ffe/siteui/vlv3/fc2c345e-5991-4917-be24-cd328b62cc3f/web_tall_panel/IN-en-20250414-TRIFECTA-perspective_0f1fb403-6efb-4223-8f10-cfd1a902f22c_large.jpg" alt='/'>
+                </img>
+                <div className="bg-black/60 h-full w-full fixed top-0 left-0"></div>
+            </div>
+        </>
+    )
 }
